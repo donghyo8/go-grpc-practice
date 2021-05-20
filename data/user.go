103718fca0c999b3a8b4acba65001969619d7e10
donghyo8 <donghyo@dgu.ac.kr>
2021-05-20 16:29:03 +0900

    simple-user rpc

--- data/user.go
+++ data/user.go
@@ -4,6 +4,9 @@ import (
 	userpb "go-grpc/protos/v1/user"
 )
 
+// 이후 DB에서 데이터를 가져오는 형식으로 변경
+// 연습이니 static하게 선언
+// user.proto에서 정의한 UserMessage 필드와 매핑되는 값들임
 var UserData = []*userpb.UserMessage{
 	{
 		UserId: "1",
--- simple-user/main.go
+++ simple-user/main.go
@@ -16,6 +16,7 @@ type userServer struct {
 	userpb.UserServer
 }
 
+// 2. user.proto에 정의한 user_id로 user의 정보를 갖고오는 rpc
 func (s *userServer) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*userpb.GetUserResponse, error) {
 	userID := req.UserId
 
@@ -33,6 +34,7 @@ func (s *userServer) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*
 	}, nil
 }
 
+// 3. user.proto에 정의한 user의 모든 정보를 조회하는 rpc
 func (s *userServer) ListUsers(ctx context.Context, req *userpb.ListUsersRequest) (*userpb.ListUsersResponse, error) {
 	userMessages := make([]*userpb.UserMessage, len(data.UserData))
 	for i, u := range data.UserData {
@@ -51,6 +53,7 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+	// 1. user_grpc.pb.go 파일의 RegisterUserServer 함수를 가져와서 user 서비스를 등록하면 user 서비스를 담당하는 grpc server가 생성됨
 	userpb.RegisterUserServer(grpcServer, &userServer{})
 
 	log.Printf("start gRPC server on %s port", portNumber)
@@ -58,3 +61,8 @@ func main() {
 		log.Fatalf("failed to serve: %s", err)
 	}
 }
+
+// BoolmRPC로 user.proto 파일을 import 하면 user.proto에 정의된 rpc들을 확인할 수 있음
+// 서버 실행 후 해당 포트로 reuqest 형식에 맞게 데이터를 request하면 response됨
+// getUser: 이미 static하게 저장해둔 데이터 필드들을 id값에 맞게 보여줌
+// ListUser: 저장된 모든 데이터들을 보여줌
